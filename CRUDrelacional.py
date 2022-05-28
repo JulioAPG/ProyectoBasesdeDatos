b86c289a5c2e34f9a569518914832c0cf7ff9338
Juan Sebastian Velez <sebasdelvelez@gmail.com>
2022-05-28 15:10:52 -0500

    Estetica y control de errores

--- CRUDrelacional.py
+++ CRUDrelacional.py
@@ -1,11 +1,29 @@
 import pymysql
+
+print("----------------------------------------------------------------")
+print("\nBienvenido al crud del sistema de cajeros \nPrimero necesitamos unos datos: \n")
+LocalhhostUser = input("Ingrese el usuario de la base de datos: ")
+LocalhhostPass = input("Ingrese la contraseña de la base de datos: ")
+print("\n----------------------------------------------------------------\n")
+
+
+class bcolors:
+    OK = '\033[92m' #GREEN
+    WARNING = '\033[93m' #YELLOW
+    FAIL = '\033[91m' #RED
+    RESET = '\033[0m' #RESET COLOR
+
+
 try:
 	conexion = pymysql.connect(host='localhost',
-                             user='root',
-                             password='TraHEY2356!',
+                             user=LocalhhostUser,
+                             password=LocalhhostPass,
                              db='transacciones')
+	print(bcolors.OK + "Conexion exitosa :D \n" + bcolors.RESET)
 except (pymysql.err.OperationalError, pymysql.err.InternalError) as e:
-	print("Ocurrió un error al conectar: ", e)       
+	print(bcolors.FAIL + "Ocurrió un error al conectar:")
+	print(e , bcolors.RESET)
+	exit()
 
 
 #llegué yooooo hptaaa
@@ -15,29 +33,40 @@ def CrearCajero(estado,modeloCajero,transacciones,zona,id_equipo):
 		consulta = "INSERT INTO cajeros(estado, modeloCajero, transacciones, zona, id_equipo) VALUES (%s, %s, %s, %s, %s);"
 		cursor.execute(consulta, (estado, modeloCajero, transacciones,zona,id_equipo))
 	conexion.commit()
+
+
 def LeerCajero():
 	with conexion.cursor() as cursorr:
 		cursorr.execute("SELECT estado, modeloCajero, zona, id_equipo FROM cajeros;")
 		cajeros = cursorr.fetchall()
 		for cajero in cajeros:
 			print(cajero)
+
+
 def EliminarCajero(id_cajero):
 	with conexion.cursor() as cursore:
 		consulta = "DELETE FROM cajeros WHERE id_equipo=%s;"
 		cursore.execute(consulta, (id_cajero))
 	conexion.commit()    
+
+
 def ModificarCajero(estado,modelo,zona,id_cajero):
 	with conexion.cursor() as cursorm:
 		consulta = "UPDATE cajeros SET estado=%s, modeloCajero=%s, zona=%s WHERE id_equipo=%s"
 		cursorm.execute(consulta, (estado,modelo,zona,id_cajero))
 	conexion.commit()
+
+
 def LeerTransacciones(id_cajero):
 	with conexion.cursor() as cursorr:
 		consulta=("SELECT transacciones FROM cajeros WHERE id_equipo=%s;")
 		cursorr.execute(consulta,(id_cajero))
 		cajeros = cursorr.fetchall()
 		for cajero in cajeros:
-			print(cajero)		    		        
+			print(cajero)		    
+
+
+
 opcion=int(input("¿Que acción desea realizar?: \n 1: Crear cajero \n 2:Visualizar los cajeros \n 3:Eliminar un cajero \n 4: Modificar un cajero \n 5: ver todas las transacciones de un cajero \n Su opción: "))
 if opcion==1:
     CrearCajero("Disponible",2015,'[{"monto": 10000,"tipoCuenta":"cuentaVirtual","tipoMovimiento":"retiro","fechaMovimiento":"17-06-2022"}]',8,"GJHGDJH")
